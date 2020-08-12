144e18cf1a012ab18a6c5ded3d15d2fe6aff9f93
Galya <galina.bosinzon@raiffeisen.ru>
2020-08-12 22:53:51 +0300

    example with all filters

--- project.py
+++ project.py
@@ -2,15 +2,5 @@ from python_code.create_dataset import create_dataset
 from python_code.kde_builder import KdeBuilder
 
 data = create_dataset('data/data.mat')
-
-kde_builder = KdeBuilder(data, 'Temperature', "season='зима' and region='средние' and SZA='день'")
-kde_builder.create_plot('images/temperature-зима-средние-день.png')
-
-kde_builder = KdeBuilder(data, 'Temperature', "season='зима' and region='средние' and SZA='ночь'")
-kde_builder.create_plot('images/temperature-зима-средние-ночь.png')
-
-kde_builder = KdeBuilder(data, 'Temperature', "season='зима' and region='полярные'")
-kde_builder.create_plot('images/temperature-зима-полярные.png')
-
-kde_builder = KdeBuilder(data, 'Temperature', "season='лето' and region='полярные'")
-kde_builder.create_plot('images/temperature-лето-полярные.png')
+kde_builder = KdeBuilder(data, 'M', "season='лето' and region='средние' and SZA='день' and F107='низкая солнечная активность'")
+kde_builder.create_plot('images/M-лето-средние-день-низкаяСА.png')
